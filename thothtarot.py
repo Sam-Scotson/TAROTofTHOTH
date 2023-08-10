6fc03a0fa9ff50e2a4466bd0c898711540dafada
Sam Scotson <118854225+Sam-Scotson@users.noreply.github.com>
2023-08-10 13:42:36 +1200

    Add files via upload

---
+++ thothtarot.py
@@ -0,0 +1,169 @@
+
+class ThothMajorArcana:
+    def __init__(self):
+        self.cards = {
+            "The Fool": {
+                "Number": 0,
+                "Zodiac Sign": "Elemental Trump of Air",
+                "Planet": "Uranus",
+                "Hebrew Letter": "Aleph (א)",
+                "Position on Tree": "Path 11 (Kether to Chokmah)",
+                "Assigned Colors": "Multicolored",
+                "Summary": "The Fool card holds a profound significance as the card of zero, representing both the unmanifest potential and the point of origin from which all other cards emanate. It symbolizes the primal, unconditioned state of existence, akin to the 'Ein Sof' or limitless divine potential. The Fool is in direct connection to Kether, the Crown, the highest Sephirah on the Tree of Life, signifying the spark of divine consciousness that initiates creation.\n\nIn the context of the hero's journey, The Fool embodies the archetype of the adventurer setting forth into the unknown. The Fool's innocence and fearlessness reflect the beginning of a transformative voyage, encountering challenges and growth along the way. This card reminds us of the boundless possibilities present when we step outside our comfort zones and embark on our personal quests."
+            },
+            "The Magus": {
+                "Number": 1,
+                "Zodiac Sign": "Messagner of the God's",
+                "Planet": "Mercury",
+                "Hebrew Letter": "Beth (ב)",
+                "Position on Tree": "Path 12 (Kether to Binah)",
+                "Assigned Colors": "Yellow, Purple",
+                "Summary": "The Magus card embodies the archetype of the divine messenger and magician. Representing the power of communication and manipulation, it signifies the harmonious union of the spiritual and material realms. The Magus wields the four elements, symbolizing mastery over the natural forces.\n\nAs the path between Kether and Binah, this card represents the transfer of divine inspiration and creative potential to the receptive and structured realm of understanding. The Magus invites us to channel our inner potential into conscious action, reminding us that our thoughts, words, and actions shape our reality."
+            },
+            "The Priestess": {
+                "Number": 2,
+                "Zodiac Sign": "Trump of the Moon",
+                "Planet": "Moon",
+                "Hebrew Letter": "Gimel (ג)",
+                "Position on Tree": "Path 13 (Kether to Tiphareth)",
+                "Assigned Colors": "Blue, Silver",
+                "Summary": "The Priestess card is a representation of the subconscious, intuition, and the hidden aspects of existence. She stands at the threshold of the unknown, embodying the mystery of the unmanifest potential. The Priestess is both a guide to the inner realms and a guardian of esoteric knowledge.\n\nAs the bridge between Kether and Tiphareth, she serves as the mediator between the divine source and the individual soul. The Priestess invites us to explore our inner landscapes, embrace our intuition, and trust the wisdom that arises from the depths of our being. This card symbolizes the delicate balance between light and shadow, revealing the power of inner reflection and the mysteries of the moonlit path."
+            },
+            "The Empress": {
+                "Number": 3,
+                "Zodiac Sign": "Venus",
+                "Planet": "Earth",
+                "Hebrew Letter": "Daleth (ד)",
+                "Position on Tree": "Path 14 (Binah to Chokmah)",
+                "Assigned Colors": "Green, Blue",
+                "Summary": "The Empress card embodies the essence of nurturing and fertility, representing the Divine Feminine and the creative force of nature. She is a symbol of abundance, growth, and the birthing of new life. As the embodiment of Venus, The Empress radiates the energy of love, beauty, and sensuality.\n\nSituated on the path between Binah and Chokmah, The Empress mediates the maternal and paternal aspects of the Tree of Life. She encourages us to connect with our intuitive and nurturing sides, embrace the cycles of creation, and appreciate the beauty and richness of the physical world. This card reminds us of our capacity to care for ourselves and others with unconditional love."
+            },
+            "The Emperor": {
+                "Number": 4,
+                "Zodiac Sign": "Aries",
+                "Planet": "Mars",
+                "Hebrew Letter": "Heh (ה)",
+                "Position on Tree": "Path 15 (Chesed to Geburah)",
+                "Assigned Colors": "Red, Yellow",
+                "Summary": "The Emperor card embodies the qualities of authority, leadership, and structure, reflecting the archetype of the Divine Masculine in its active and assertive form. As the embodiment of Mars, The Emperor exudes courage, determination, and the will to take action.\n\nOn the path between Chesed and Geburah, The Emperor serves as a bridge between expansion and restriction. He invites us to establish order, set boundaries, and exercise our power responsibly. This card symbolizes the importance of balance between asserting our authority and respecting the rights and needs of others. The Emperor encourages us to take charge of our lives and manifest our visions with discipline and strength."
+            },
+            "The Hierophant": {
+                "Number": 5,
+                "Zodiac Sign": "Taurus",
+                "Planet": "Jupiter",
+                "Hebrew Letter": "Vav (ו)",
+                "Position on Tree": "Path 16 (Chesed to Tiphareth)",
+                "Assigned Colors": "Yellow, Blue",
+                "Summary": "The Hierophant card embodies spiritual guidance, tradition, and the transmission of divine wisdom. He represents the bridge between the earthly and spiritual realms, serving as a conduit for higher teachings and sacred rituals. The Hierophant is a symbol of religious and spiritual authority.\n\nAs a path between Chesed and Tiphareth, The Hierophant integrates spiritual values with compassionate action. This card invites us to seek guidance from spiritual traditions, honor our inner wisdom, and embrace the role of mentor and seeker. The Hierophant reminds us that connection to the divine can be found through both external teachings and inner exploration."
+            },
+            "The Lovers": {
+                "Number": 6,
+                "Zodiac Sign": "Gemini",
+                "Planet": "Mercury",
+                "Hebrew Letter": "Zayin (ז)",
+                "Position on Tree": "Path 17 (Geburah to Tiphareth)",
+                "Assigned Colors": "Red, Blue",
+                "Summary": "The Lovers card represents the harmony and integration of opposing forces. It embodies the concept of duality and the potential for union and balance. The Lovers card speaks to the deep connections we form, whether in relationships, choices, or inner struggles.\n\nAs a path between Geburah and Tiphareth, The Lovers card signifies the transformative journey from severity to beauty. This card encourages us to make choices that align with our values and higher selves, fostering unity within and without. The Lovers remind us that true love and union are rooted in authenticity and the recognition of our interconnectedness."
+            },
+
+        }
+        
+
+thoth_major_arcana = ThothMajorArcana()
+
+magus_info = thoth_major_arcana.cards["The Magus"]
+print("Number:", magus_info["Number"])
+print("Zodiac Sign:", magus_info["Zodiac Sign"])
+print("Planet:", magus_info["Planet"])
+print("Summary:", magus_info["Summary"])
+
+
+
+
+
+
+
+
+
+'''"The Fool": "",
+"The Magus": "",
+"The Priestess": "",
+"The Empress": "",
+"The Emperor": "",
+"The Hierophant": "",
+"The Lovers": "",
+"Adjustment": "",
+"The Hermit": "",
+"Fortune": "",
+"Lust": "",
+"The Hanged Man": "",
+"Death": "",
+"Art": "",
+"The Devil": "",
+"The Tower": "",
+"The Star": "",
+"The Moon": "",
+"The Sun": "",
+"The Aeon": "",
+"The Universe": "",
+"Ace of Cups": "",
+"Two of Cups": "",
+"Three of Cups": "",
+"Four of Cups": "",
+"Five of Cups": "",
+"Six of Cups": "",
+"Seven of Cups": "",
+"Eight of Cups": "",
+"Nine of Cups": "",
+"Ten of Cups": "",
+"Knight of Cups": "",
+"Queen of Cups": "",
+"Prince of Cups": "",
+"Princess of Cups": "",
+"Ace of Wands": "",
+"Two of Wands": "",
+"Three of Wands": "",
+"Four of Wands": "",
+"Five of Wands": "",
+"Six of Wands": "",
+"Seven of Wands": "",
+"Eight of Wands": "",
+"Nine of Wands": "",
+"Ten of Wands": "",
+"Knight of Wands": "",
+"Queen of Wands": "",
+"Prince of Wands": "",
+"Princess of Wands": "",
+"Ace of Swords": "",
+"Two of Swords": "",
+"Three of Swords": "",
+"Four of Swords": "",
+"Five of Swords": "",
+"Six of Swords": "",
+"Seven of Swords": "",
+"Eight of Swords": "",
+"Nine of Swords": "",
+"Ten of Swords": "",
+"Knight of Swords": "",
+"Queen of Swords": "",
+"Prince of Swords": "",
+"Princess of Swords": "",
+"Ace of Disks": "",
+"Two of Disks": "",
+"Three of Disks": "",
+"Four of Disks": "",
+"Five of Disks": "",
+"Six of Disks": "",
+"Seven of Disks": "",
+"Eight of Disks": "",
+"Nine of Disks": "",
+"Ten of Disks": "",
+"Knight of Disks": "",
+"Queen of Disks": "",
+"Prince of Disks": "",
+"Princess of Disks": ""'''
+
+
+
+
+
